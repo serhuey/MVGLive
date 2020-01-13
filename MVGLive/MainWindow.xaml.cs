df98363df1895d24b6e33c6794720db506583743
Sergei Grigorev <s.v.grigoryev@gmail.com>
2020-01-13 11:49:19 +0100

    Some icons are changed, added new converter for colorize warning strings.

--- MVGLive/MainWindow.xaml.cs
+++ MVGLive/MainWindow.xaml.cs
@@ -30,7 +30,7 @@ namespace MVGLive
             }
 
             // ToDo: Move all this parameters from code to the settings form or settings xml-file
-            fontSize = 30;
+            fontSize = 32;
             //string fontFamily = "PT Sans";
             string fontFamily = "Segoe UI";
             string defaultDirection1 = "Hirschgarten";
---
+++ MVGTimeTable/Converters/MainDestinationForegroundMVC.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MVGTimeTable
+{
+    public class MainDestinationForegroundMVC : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values == null || values.Length == 0) return null;
+
+            SolidColorBrush brush;
+            string foregroundColor;
+            string product = values[0].ToString();
+            switch (product)
+            {
+                case ("WARNING"):
+                    foregroundColor = "#FFFFEB85"; break;
+                case ("NO_CONNECTION"):
+                    foregroundColor = "#FFFF4E48"; break;
+                default:
+                    foregroundColor = "#FFE8E8E8"; break;
+            }
+            brush = new SolidColorBrush((Color)ColorConverter.ConvertFromString(foregroundColor));
+            return brush;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
